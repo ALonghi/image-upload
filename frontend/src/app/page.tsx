ec0dc229ef4b1fec194f40031b778cf2d50d908e
Alessio Longhi <alonghii@icloud.com>
2024-01-03 12:10:54 +0100

    Show uploaded pictures

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react'
 
 export default function Home() {
   const [response, setResponse] = useState<string>(``)
+  const [images, setImages] = useState<string[]>([])
   const testData = () => {
     axios
       .get(`${process.env.NEXT_PUBLIC_API_URL}/`)
@@ -12,18 +13,25 @@ export default function Home() {
       .catch(err => alert(err))
   }
 
-  const listObjects = () => {
-    axios
+  const listObjects = async () => {
+    await axios
       .get(`${process.env.NEXT_PUBLIC_API_URL}/list`)
-      .then(res => setResponse(JSON.stringify(res.data, null, 2)))
+      .then(res => {
+        setResponse(JSON.stringify(res.data, null, 2))
+        setImages(res.data.data)
+      })
       .catch(err => alert(err))
+  }
+
+  const addImage = (imageUrl) => {
+    setImages(prev => [...prev, imageUrl])
   }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-y-12 p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex h-fit">
         <p>Backend url: {process.env.NEXT_PUBLIC_API_URL}</p>
-        <ImageUpload />
+        <ImageUpload setResponse={setResponse} addImage={addImage} />
         <button type="button"
           onClick={() => testData()}
           className='disabled:cursor-not-allowed disabled:border-red-600 py-2 px-8 rounded-lg border-gray-700 border hover:bg-gray-700 hover:text-white'>
@@ -37,6 +45,12 @@ export default function Home() {
       <div className='mx-auto bg-gray-200 rounded-lg p-4'>
         <pre className='text-xs'>{response}</pre>
       </div>
+      <p>Length: {images.length || 0}</p>
+      {images.length > 0 && (
+        <div className='mt-2 w-full flex flex-wrap gap-3 bg-gray-200 rounded-lg p-4'>
+          {images.map((url, index) => <img src={url} key={index} className='w-44 h-auto object-cover' />)}
+        </div>
+      )}
     </main>
   )
 }
--- frontend/src/components/ImageUpload.tsx
+++ frontend/src/components/ImageUpload.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const ImageUpload = () => {
+const ImageUpload = ({ setResponse, addImage }) => {
     const [image, setImage] = useState(null);
 
     const handleImageChange = (e) => {
@@ -20,18 +20,22 @@ const ImageUpload = () => {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
+            }).then(res => {
+                setResponse(JSON.stringify(res.data, null, 2))
+                addImage(res.data.data)
             });
             alert('Image uploaded successfully');
         } catch (error) {
             console.error('Error uploading image', error);
+            alert(error)
         }
     };
 
     return (
         <form onSubmit={handleSubmit} className='flex gap-x-8'>
             <input type="file" onChange={handleImageChange} className='' />
-            <button type="submit" disabled={image === null} 
-            className='disabled:cursor-not-allowed disabled:border-red-600 py-2 px-8 rounded-lg border-gray-700 border hover:bg-gray-700 hover:text-white'>Upload Image</button>
+            <button type="submit" disabled={image === null}
+                className='disabled:cursor-not-allowed disabled:border-red-600 py-2 px-8 rounded-lg border-gray-700 border hover:bg-gray-700 hover:text-white'>Upload Image</button>
         </form>
     );
 };
